2650b2d2168af6728dbaee6bb21089bac6d8ec94
Sudheer Amgothu <Sudheer.amgothu@ellevationeducation.com>
2017-10-23 13:10:13 -0400

    Added my_strcpy function

--- string.c
+++ string.c
@@ -8,11 +8,27 @@ int my_strlen(char *s)
       ++p;
 
    size_t my_strlen(char *s)
+return (p-s);
+}
+
+char *my_strcpy(char *t, char *s)
+
+{
+   
+   char *p = t;
+   
+    while (*t++ = *s++)
+    ;
+   
+   
+   return p;
+   
 }
 
 int main(void)
 {
    int i;
+   char p1[32];
    char *s[] = 
    {
       "Git tutorials",
@@ -22,6 +38,7 @@ int main(void)
    for (i = 0; i < 2; ++i)
       
    printf("string lenght of %s = %lu\n", s[i], my_strlen(s[i]));
+   printf("%s\n", my_strcpy(p1, "Hello, World !!!"));
 
    return 0;
 }
